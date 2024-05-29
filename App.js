7504616e4de03a0910025a262c530be0ffc44dc3
Allam9573 <aallamdaniel@gmail.com>
2024-05-29 14:40:44 -0600

    pantalla

--- App.js
+++ App.js
@@ -5,6 +5,11 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.con}>
+        <Text >
+          Consult only with a doctor you trust!
+        </Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +18,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: 'white',
     justifyContent: 'center',
+    alignItems: 'center'
   },
+  con: {
+    backgroundColor: '#7E8EF1',
+    width: '90%',
+    height: '30%',
+    position: 'absolute',
+    bottom: 0,
+    borderTopEndRadius: 10,
+    borderTopStartRadius: 10,
+    elevation: 5
+  }
 });
